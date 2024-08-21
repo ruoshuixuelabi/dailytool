ee723cf4349a01b49127f23993ddef582babefd3
mazhenkun <mazk@lab.rhzz.com.cn>
2024-08-21 10:02:53 +0800

    修改代码

--- src/main/java/com/xuexi/dailytool/file/FileRename.java
+++ src/main/java/com/xuexi/dailytool/file/FileRename.java
@@ -2,64 +2,39 @@ package com.xuexi.dailytool.file;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
-/**
- * @author admin
- */
-public class Man {
+@Slf4j
+public class FileRename {
     public static void main(String[] args) throws IOException {
-        Stream<Path> list = Files.list(Paths.get("H:\\深入剖析阿里核心微服务技术Spring Cloud Alibaba\\视频"));
-        list.forEach(path -> {
-            rename(path, "【瑞客论坛 www.ruike1.com】");
-//            FileUtil.rename(path.toFile(), StrUtil.removeAll(path.toFile().toString(), "【瑞客论坛 www.ruike1.com】"), false, true);
-        });
+        try (Stream<Path> list = Files.list(Paths.get("H:\\深入剖析阿里核心微服务技术Spring Cloud Alibaba\\视频"))) {
+            list.forEach(path -> rename(path, "【瑞客论坛 www.ruike1.com】"));
+        }
     }
 
     public static void rename(Path path, String name) {
         if (FileUtil.isDirectory(path.toFile())) {
-            File rename = FileUtil.rename(path.toFile(), StrUtil.removeAll(path.toFile().toString(), name), false, true);
-            File[] files = rename.listFiles();
+            File[] files = path.toFile().listFiles();
             if (files != null) {
-                for (File file : files) {
-                    rename(file.toPath(), name);
-                }
+                Arrays.stream(files).map(File::toPath).forEach(p -> rename(p, name));
             }
         } else {
-            try {
-                FileUtil.rename(path.toFile(), StrUtil.removeAll(path.toFile().toString(), name), false, true);
-            } catch (Exception e) {
-
+            String fileName = path.getFileName().toString();
+            if (fileName.contains(name)) {
+                try {
+                    FileUtil.rename(path.toFile(), StrUtil.removeAll(path.toFile().toString(), name), false, true);
+                } catch (Exception e) {
+                    log.error("重命名失败: {}", e.getMessage(), e);
+                }
             }
         }
     }
-
-    private Godness god;
-
-    public Man() {
-    }
-
-    public Man(Godness god) {
-        this.god = god;
-    }
-
-    public Godness getGod() {
-        return god;
-    }
-
-    public void setGod(Godness god) {
-        this.god = god;
-    }
-
-    @Override
-    public String toString() {
-        return "Man [god=" + god + "]";
-    }
-
 }
